4b16982fe00a03f9a97c95bbf2f8be8f2630af96
Yuriy Fundak <yfundak@gmail.com>
2025-09-16 22:53:10 +0300

    add models

---
+++ tierlist-server/TierlistServer/Models/TierList.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TierlistServer.Models
+{
+    [Table("tier_lists")]
+    public class TierList
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("title")]
+        [StringLength(200)]
+        public string Title { get; set; } = "My Tier List";
+
+        [Required]
+        [ForeignKey("User")]
+        [Column("user_id")]
+        public int UserId { get; set; }
+
+        public User? User { get; set; }
+
+        public ICollection<Game> Games { get; set; } = new List<Game>();
+    }
+}
