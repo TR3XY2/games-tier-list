1c9696877472acea87a35f1e7034da950382d60e
Yuriy Fundak <yfundak@gmail.com>
2025-09-28 15:46:00 +0300

    add repos scoped to app

--- tierlist-server/src/TierlistServer.Api/Program.cs
+++ tierlist-server/src/TierlistServer.Api/Program.cs
@@ -1,13 +1,24 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using TierlistServer.Application.Services;
 using TierlistServer.Domain.Entities;
+using TierlistServer.Domain.Interfaces;
+using TierlistServer.Infrastructure;
 using TierlistServer.Infrastructure.Data;
+using TierlistServer.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<TierlistDbContext>(options =>
-options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IGameRepository, GameRepository>();
+builder.Services.AddScoped<ITierListRepository, TierListRepository>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+builder.Services.AddScoped<UserService>();
+
 builder.Services.AddControllers();
 builder.Services.AddCors(options =>
 {
